2428b6f37090188dfe174aaa72d8e053dabbb13f
ArthurOstr <automata4231@gmail.com>
2026-05-23 22:58:22 +0300

    fix: deleting interceptor in client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -8,18 +8,6 @@ const apiClient = axios.create({
     withCredentials: true,
 });
 
-apiClient.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        if (error.response && error.response.status === 401) {
-            window.location.href='/';
-        }
-        return Promise.reject(error);
-    }
-)
-
 export const getTransactions = async (): Promise<Transaction[]> => {
   const response = await apiClient.get(`/transactions`);
   return response.data;
